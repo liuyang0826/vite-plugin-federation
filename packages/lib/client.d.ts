10a6c2e137cc78ae2df270ea3846e32ef86980ee
liuyang.0826 <1161906592@qq.com>
2023-09-05 17:55:20 +0800

    fix: `client.d.ts` error

--- packages/lib/client.d.ts
+++ packages/lib/client.d.ts
@@ -1,6 +1,5 @@
-import type { RemotesConfig } from './types'
-
 declare module '~federation' {
+  import type { RemotesConfig } from '@liuyang0826/vite-plugin-federation'
   function setRemote(remoteName: string, remoteConfig: RemotesConfig): void
   function getRemote(remoteName: string, componentName: string): Promise<any>
   export { getRemote, setRemote }
