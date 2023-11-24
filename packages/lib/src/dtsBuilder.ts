441ecfe8e70e5b4412fa74127c40745ff36ca170
liuyang.0826 <1161906592@qq.com>
2023-11-24 10:16:30 +0800

    fix: dts match error

--- packages/lib/src/dtsBuilder.ts
+++ packages/lib/src/dtsBuilder.ts
@@ -4,14 +4,12 @@ import type { Context } from 'types'
 
 export default async function dtsBuilder(context: Context) {
   try {
-    const input = context.expose
-      .filter((item) => item[1].types)
-      .map((item) => item[1].types!)
+    const input = context.expose.filter((item) => item[1].types)
     if (!input.length) return []
 
     const build = await rollup({
       plugins: [dts()],
-      input: input
+      input: input.map((item) => item[1].types!)
     })
 
     const { output: outputs } = await build.generate({ format: 'esm' })
@@ -19,7 +17,7 @@ export default async function dtsBuilder(context: Context) {
 
     outputs.forEach((output, index) => {
       if (output.type === 'chunk') {
-        body.push({ name: context.expose[index][0], code: output.code })
+        body.push({ name: input[index][0], code: output.code })
       }
     })
 
